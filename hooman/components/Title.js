29a46b7d3fddbbcebb11712f29f6cb60316e0194
Dan Chang <dmagma@hotmail.com>
2021-06-15 12:13:46 -0700

    Cleanup unused code

--- hooman/components/Title.js
+++ hooman/components/Title.js
@@ -8,12 +8,7 @@ const Title = () => {
           Title
         </div>
         <div className={styles.titleBackground}>
-          {/* <div className={styles.heading}>
-            Zhang v. USCIS: the profound impact of loans as a source of funds for investors investors
-          </div> */}
           <form>
-            {/* <input type="text-area" name="title" value="Zhang v. USCIS: the profound impact of loans as a source of funds for investors investors">
-            </input> */}
             <textarea 
               id="title"
               name="title"
